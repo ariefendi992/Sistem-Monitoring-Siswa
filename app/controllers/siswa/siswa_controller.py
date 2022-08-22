d191962007248dca1a1c16ca82e2bc996e24e093
ari efendi <ari@aris-MacBook-Pro.local>
2022-08-22 23:28:46 +0800

    update struktur folder

--- app/controllers/siswa/siswa_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from app.models.siswa_model import SiswaModel
-
-siswa = Blueprint('siswa', __name__, url_prefix='/siswa')
-
-@siswa.post('/add-new-siswa')
-def add_new_siswa():
-    pass
